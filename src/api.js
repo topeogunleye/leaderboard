673d5daa68f4b9255f52c5d293f343c2eac467d2
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-15 16:22:33 +0100

    Make Methods of game class async

--- src/api.js
+++ src/api.js
@@ -1,25 +1,24 @@
 const url =
-  ' https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6yqVLFpP806Nk1GJG4vd/scores';
-
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6yqVLFpP806Nk1GJG4vd/scores';
 class game {
-  static getScore() {
+  static async getScore() {
     const res = await fetch(url);
     const data = await res.json();
     return data.result;
   }
 
   // POST request
-  static addScore (obj) {
+  static async addScore(obj) {
     const res = await fetch(url, {
-       method: 'POST',
-       body: JSON.stringify(obj),
-       headers: {
-         'Content-Type': 'application/json',
-       }, // end headers
-     })
-     const data = await res.json();
-     return data.result;
-   }
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+    const data = await res.json();
+    return data.result;
+  }
 }
 
 export default game;
