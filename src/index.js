c45278ad07c6955bc54d30ad325c6d4ce963907a
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-15 16:34:07 +0100

    Get user inputs on submit button click event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.scss';
+import game from './api';
+import leaderboard from './board';
+
+//event listeners on submit button
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const user = document.querySelector('#name').value;
+  const score = Number(document.querySelector('#score').value);
+  const newScore = {
+    user,
+    score,
+  };
+
+  game.addScore(newScore);
+
+  //clear input fields
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
+});
+
